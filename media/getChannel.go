d415b2884fc47267f5d4985c07e7a55793542d9e
takaaki-mapbox <takaaki.kuwabara@mapbox.com>
2022-08-02 17:24:33 +0900

    fix

--- media/getChannel.go
+++ media/getChannel.go
@@ -6,22 +6,22 @@ import (
 	"gps_logger/logger"
 )
 
-func (m *Media) GetChannel() (string, string, error) {
+func (m *Media) GetChannel() (string, string, string, error) {
 	channels, err := m.GetChannelsFromRedis()
 	if err != nil {
 		logger.Error("Failed to get channels from redis. err: %v", err)
-		return "", "", err
+		return "", "", "", err
 	}
-	for _, value := range channels {
+	for key, value := range channels {
 		channel := &Channel{}
 		if err := json.Unmarshal([]byte(value), &channel); err != nil {
 			logger.Error("Failed unmarshal value. err: %v", err)
-			return "", "", err
+			return "", "", "", err
 		}
 		if !channel.IsUsed {
-			return channel.Id, channel.Url, nil
+			return key, channel.Id, channel.Url, nil
 		}
 	}
 	logger.Info("There is not unsable channel, all channels are used already.")
-	return "", "", errors.New("there is not unsable channel")
+	return "", "", "", errors.New("there is not unsable channel")
 }
--- media/media.go
+++ media/media.go
@@ -16,7 +16,7 @@ type Media struct {
 
 type MediaRepository interface {
 	CreateChannel() (string, error)
-	GetChannel() (string, string, error)
+	GetChannel() (string, string, string, error)
 	describeContainer(containerName string) (*mediastore.DescribeContainerOutput, error)
 	describeChannel(channelId string) (*medialive.DescribeChannelOutput, error)
 	StartChannel(userId string, channelId string) error
