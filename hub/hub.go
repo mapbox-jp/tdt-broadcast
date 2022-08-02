800600079ed4f763acb1b80f79de46582da2a5bb
takaaki-mapbox <takaaki.kuwabara@mapbox.com>
2022-08-03 01:10:54 +0900

    fix

--- hub/hub.go
+++ hub/hub.go
@@ -68,14 +68,14 @@ func (h *Hub) Run() {
 			})
 
 			jsonBytes, _ := json.Marshal(&Response{
-				Type: "JOINED",
+				Type:  "PING",
+				Url:   "",
+				Error: nil,
 			})
 			rider.Send <- jsonBytes
 
 			jsonBytes, _ = json.Marshal(&Response{
-				Type:  "PING",
-				Url:   "",
-				Error: nil,
+				Type: "JOINED",
 			})
 			rider.Send <- jsonBytes
 
@@ -108,18 +108,18 @@ func (h *Hub) Run() {
 			serialized, _ := json.Marshal(location)
 			h.Rd.HSet("locations", userId, serialized)
 		case <-ticker.C:
-			for rider := range h.Riders {
-				jsonBytes, err := json.Marshal(&Response{
-					Type:  "PING",
-					Url:   "",
-					Error: nil,
-				})
-				if err != nil {
-					return
-				}
-				logger.Info("ping")
-				rider.Send <- jsonBytes
-			}
+			// for rider := range h.Riders {
+			// 	jsonBytes, err := json.Marshal(&Response{
+			// 		Type:  "PING",
+			// 		Url:   "",
+			// 		Error: nil,
+			// 	})
+			// 	if err != nil {
+			// 		return
+			// 	}
+			// 	logger.Info("ping")
+			// 	rider.Send <- jsonBytes
+			// }
 
 			if len(h.Broadcasts) > 0 {
 				jsonBytes, err := json.Marshal(h.Broadcasts)
